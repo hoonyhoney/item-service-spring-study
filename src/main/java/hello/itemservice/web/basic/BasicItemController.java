1c29c2fba5107153c8f4c9b16bce6fc8d69eb203
hoonyhoney <hoonyhoeny@gmail.com>
2023-04-22 22:00:41 +0900

    Initial commit

---
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -0,0 +1,41 @@
+package hello.itemservice.web.basic;
+
+import hello.itemservice.domain.item.Item;
+import hello.itemservice.domain.item.ItemRepository;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/basic/items")
+public class BasicItemController {
+
+
+
+    private final ItemRepository itemRepository;
+
+    @Autowired
+    public BasicItemController(ItemRepository itemRepository) {
+        this.itemRepository = itemRepository;
+    }
+
+    @GetMapping
+    public String itemsj(Model model){
+        List<Item> items = itemRepository.findAll();
+        model.addAttribute("items",items);
+        return "basic/items";
+    }
+
+    @PostConstruct
+    public void init(){
+        itemRepository.save(new Item("itemA",1000,10));
+        itemRepository.save(new Item("itemB",2000,20));
+    }
+
+
+}
